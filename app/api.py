a87e43affc9b93307d158a42f92495dc69ab5547
Justin <hyp3rstreak@gmail.com>
2026-02-22 15:21:53 -0500

    forecast calls

--- app/api.py
+++ app/api.py
@@ -37,3 +37,46 @@ def current_weather(city: str):
         "windspeed": current["windspeed"],
         "time": current["time"]
     }
+
+@app.get("/forecast")
+def forecast(city: str):
+
+    geo_url = "https://geocoding-api.open-meteo.com/v1/search"
+    geo_response = requests.get(
+        geo_url,
+        params={"name": city, "count": 1}
+    ).json()
+
+    if "results" not in geo_response:
+        raise HTTPException(status_code=404, detail="City not found")
+
+    location = geo_response["results"][0]
+
+    forecast_url = "https://api.open-meteo.com/v1/forecast"
+    weather_response = requests.get(
+        forecast_url,
+        params={
+            "latitude": location["latitude"],
+            "longitude": location["longitude"],
+            "daily": ["temperature_2m_max", "temperature_2m_min"],
+            "temperature_unit": "fahrenheit",
+            "forecast_days": 5,
+            "timezone": "auto"
+        }
+    ).json()
+
+    daily = weather_response["daily"]
+
+    forecast_data = []
+
+    for i in range(len(daily["time"])):
+        forecast_data.append({
+            "date": daily["time"][i],
+            "max_temp": daily["temperature_2m_max"][i],
+            "min_temp": daily["temperature_2m_min"][i]
+        })
+
+    return {
+        "city": location["name"],
+        "forecast": forecast_data
+    }
